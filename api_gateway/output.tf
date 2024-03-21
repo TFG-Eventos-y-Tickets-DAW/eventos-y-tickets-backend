7d494849d98d2e10662e96f5739e8d800141f37e
Brayan Ayala <abryan172@gmail.com>
2024-03-21 21:52:40 +0100

    output api gateway endpoint

--- api_gateway/output.tf
+++ api_gateway/output.tf
@@ -1,3 +1,7 @@
 output "api_gateway_execution_arn" {
   value = module.api_gateway.apigatewayv2_api_execution_arn
 }
+
+output "api_gateway_endpoint" {
+  value = module.api_gateway.apigatewayv2_api_api_endpoint
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "api_gateway_endpoint" {
+  value = module.api_gateway.api_gateway_endpoint
+}
